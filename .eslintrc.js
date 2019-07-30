51c09514fb7fab56c64acfacd3779cd63ab691fd
mouse_484 <escmouse484@gmail.com>
2019-07-30 09:36:15 +0000

    eslint default setting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        "browser": true,
+        "es6": true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:prettier/recommended"
+    ],
+     rules: {
+        "prettier/prettier": "error",
+    }
+};
